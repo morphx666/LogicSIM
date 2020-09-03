c205197f6607cb08ed0c98dab7ad5753bc8ab088
Xavier Flix <xavier@xfx.net>
2020-09-03 17:31:47 -0400

    Updated to .NET 4.8

--- LogicSIM/My Project/AssemblyInfo.vb
+++ LogicSIM/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2020.9.3.874")>
-<Assembly: AssemblyFileVersion("2020.9.3.874")>
+<Assembly: AssemblyVersion("2020.9.3.875")>
+<Assembly: AssemblyFileVersion("2020.9.3.875")>
